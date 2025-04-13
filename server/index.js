13e8dbcc3168739b521a894ffe7b1508d6227bf1
Jeffery Bennett <meandmybadself@gmail.com>
2025-04-12 22:07:12 -0500

    Changes default port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import parser from 'cron-parser';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Array of cron expressions
 const cronSchedules = [
